9a55f9f705a047a769451f3ec3bc6b44e787e70c
Sean Murphy <sean@iamseanmurphy.com>
2026-04-15 12:37:47 +0000

    3.3.3.4

--- advanced-product-labels-for-woocommerce/woocommerce-advanced-products-labels.php
+++ advanced-product-labels-for-woocommerce/woocommerce-advanced-products-labels.php
@@ -3,17 +3,17 @@
  * Plugin Name: WooCommerce Advanced Product Labels
  * Plugin URI: https://berocket.com/woocommerce-advanced-product-labels?utm_source=paid_plugin&utm_medium=plugins&utm_campaign=products_label
  * Description: Promote your products! Show “Free Shipping” or other special attributes with your products.
- * Version: 3.3.3.3
+ * Version: 3.3.3.4
  * Author: BeRocket
  * Requires at least: 5.0
- * Tested up to: 6.9.3
+ * Tested up to: 6.9.4
  * Requires PHP: 7.0
  * Requires Plugins: woocommerce
  * Author URI: https://berocket.com?utm_source=paid_plugin&utm_medium=plugins&utm_campaign=products_label
  * Text Domain: BeRocket_products_label_domain
  * Domain Path: /languages/
- * WC tested up to: 10.6
+ * WC tested up to: 10.6.2
  */
-define( "BeRocket_products_label_version", '3.3.3.3' );
+define( "BeRocket_products_label_version", '3.3.3.4' );
 define( "BeRocket_products_label_file", __FILE__ );
 include_once('main.php');
